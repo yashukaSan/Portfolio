7fe4f51ebe123336951efa280e006a7a934c355c
yashukaSan <ysahu1590@gmail.com>
2026-05-04 23:10:45 +0530

    Styled in the Hero Page ONLY

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -1,19 +1,22 @@
 
 export default function Hero(){
     return (
-        <section>
-            <section>
-                <div>
-                    MERN STACK SOFTWARE DEVELOPER
+        <section >
+            <section className="bg-radial-[at_50%_35%] from-[#235940] via-[#022458] to-[#082539] h-screen flex flex-col justify-center mt-[100px] relative items-center align-center text-center" >
+                <div className="text-[#e2e3e0] text-7xl mb-12 flex flex-wrap font-bold justify-center" >
+                    MERN Stack <br />
+                    <span className="bg-linear-r bg-clip-text from-[rgb(0,100,90)] via-white to-[rgb(0,12,100)] " >
+                        Software Developer
+                    </span>
                 </div>
-                <p>
+                <p className="text-2xl p-2 font-semibold text-[#afafaf] leading-relaxed " >
                     Crafting fluid digital experience with MongoDB, Express, React, and Node.js. Engineering scable solution with modern aesthetics.
                 </p>
-                <div>
-                    <button>
+                <div className="m-12 flex gap-12" >
+                    <button className="border px-7 py-5 rounded-3xl font-semibold bg-linear-to-tr from-cyan-400 via-[rgb(0,100,090)] to-cyan-500 text-2xl "> 
                         View All Projects
                     </button>
-                    <button>
+                    <button className="border px-8 rounded-3xl text-2xl">
                         Get In Touch
                     </button>
                 </div>
